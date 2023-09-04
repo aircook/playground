a2568dc4220e4eaac3d721c20bc40b9687316749
francis.lee <aircook@gmail.com>
2023-09-04 16:42:14 +0900

    Project settings of spring boot (kotlin)

---
+++ src/test/kotlin/com/tistory/aircook/test/TestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tistory.aircook.test
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
