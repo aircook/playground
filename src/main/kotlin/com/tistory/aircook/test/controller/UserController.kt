446496fc21960d222021228e710e40c289840c69
francis.lee <aircook@gmail.com>
2023-09-04 17:19:40 +0900

    Create sample code for PathVariable

--- src/main/kotlin/com/tistory/aircook/test/controller/UserController.kt
+++ src/main/kotlin/com/tistory/aircook/test/controller/UserController.kt
@@ -1,9 +1,6 @@
 package com.tistory.aircook.test.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.Optional;
 
 @RestController
@@ -13,6 +10,11 @@ class UserController {
     @GetMapping
     fun getUser(@RequestParam(required = false) name: Optional<String>): String {
         return "Hello ${name.orElse("World")}"
+    }
+
+    @GetMapping("/{idx}")
+    fun getUserDetail(@PathVariable("idx") idx: Int): String {
+        return "Hello User Detail $idx"
     }
 
 }
