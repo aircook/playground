6eda844890c529c21c2c5c9da98042ba7029db0a
francis.lee <aircook@gmail.com>
2023-10-25 11:02:46 +0900

    Convert from gradle to maven

--- src/main/kotlin/com/tistory/aircook/playground/repository/PeopleRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.tistory.aircook.playground.repository
-
-import com.tistory.aircook.playground.entity.People
-import org.springframework.data.repository.CrudRepository
-
-interface PeopleRepository : CrudRepository<People, Long> {
-}
