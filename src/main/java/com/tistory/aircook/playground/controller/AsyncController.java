57512b33e03110af166bc2dc514b1bc1095d3b16
francis.lee <aircook@gmail.com>
2025-06-26 18:18:38 +0900

    Add test code for async 3

--- src/main/java/com/tistory/aircook/playground/controller/AsyncController.java
+++ src/main/java/com/tistory/aircook/playground/controller/AsyncController.java
@@ -2,14 +2,26 @@ package com.tistory.aircook.playground.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.StopWatch;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.MessageFormat;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+/**
+ *
+ * thenApply() 데이터 변형, map
+ * thenAccept() 데이터 소비 (출력/저장), forEach
+ * thenRun() 결과와 무관한 후속 실행
+ * thenCompose() 앞의 결과로 다음 비동기 작업 연동, flatMap
+ * thenCombine() 두 작업 결과를 병렬로 조합
+ *
+ * @author : francis.lee
+ * @since : 2025-06-26
+ */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/async")
@@ -96,7 +108,6 @@ public class AsyncController {
         log.debug("Thread 5: [{}]", Thread.currentThread().getName());
 
         return "completed";
-
     }
 
     //1 --> 5 --> 2 --> 3 --> 4
@@ -131,6 +142,46 @@ public class AsyncController {
 
         return "completed";
 
+    }
+
+    //http://localhost:8080/async/supply-async-then-apply-exceptionally
+    @GetMapping("/supply-async-then-apply-exceptionally")
+    public String supplyAsyncThenApplyExceptionally() throws ExecutionException, InterruptedException {
+
+        log.debug("Thread 1: [{}]", Thread.currentThread().getName());
+        //메인쓰레드 3초지연
+        this.delayTask(3_000);
+
+        //supplyAsync, 반환값이 있는 경우, 비동기로 작업 실행
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+            log.debug("Thread 2: [{}]", Thread.currentThread().getName());
+            //비동기 쓰레드 3초 지연
+            this.delayTask(3_000);
+            log.debug("Thread 3: [{}]", Thread.currentThread().getName());
+            return "Async Call is Completed";
+        })
+        //thenApply는 값을 받아서 다른 값을 반환
+        .thenApply(s -> {
+            log.debug("Thread 4: [{}]", Thread.currentThread().getName());
+            if (StringUtils.countOccurrencesOf(s, "Async") > 0) {
+                throw new RuntimeException(MessageFormat.format("Consumer 내부에서 예외 [{0}]", s.toUpperCase()));
+            }
+            //대문자로 변경
+            return s.toUpperCase();
+        })
+        //예외처리
+        .exceptionally(t -> {
+            log.debug("Thread 5: [{}]", Thread.currentThread().getName());
+            t.printStackTrace();
+            return t.getMessage().toUpperCase();
+        });
+
+        //메인쓰레드에서 값이 필요하면 get()사용, 하지만 blocking이 된다.
+        //String asyncResult = future.get();
+        //log.debug("Return result is [{}]", asyncResult);
+        log.debug("Thread 6: [{}]", Thread.currentThread().getName());
+
+        return "completed";
     }
 
 
