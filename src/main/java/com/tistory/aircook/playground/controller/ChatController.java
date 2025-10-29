06c55750babb024b569b1c7ab9f21b60791c81a2
francis.lee <aircook@gmail.com>
2025-10-29 15:57:16 +0900

    Change chat message format
    보낸사람과 시간 추가

--- src/main/java/com/tistory/aircook/playground/controller/ChatController.java
+++ src/main/java/com/tistory/aircook/playground/controller/ChatController.java
@@ -1,21 +1,36 @@
 package com.tistory.aircook.playground.controller;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import java.time.LocalDateTime;
+
 @Controller
 @Slf4j
 public class ChatController {
+
+    @Data
+    public static class ChatMessage {
+        private String sender;
+        private String message;
+        private LocalDateTime timestamp;
+    }
 
     // 클라이언트에서 /app/chat으로 보낸 메시지를 처리
     @MessageMapping("/chat")
     // 처리 후 /topic/messages로 브로드캐스트
     @SendTo("/topic/messages")
-    public String sendMessage(String message) {
-        log.debug("클라이언트로 부터 받은 메시지: {}", message);
-        // 서버에서 받은 메시지를 그대로 클라이언트로 전송
-        return "Server: " + message;
+    public ChatMessage sendMessage(ChatMessage chatMessage) {
+        // 서버에서 수신 시 timestamp 설정
+        chatMessage.setTimestamp(LocalDateTime.now());
+        log.debug("Message from client(browser): {}", chatMessage);
+        // 클라이언트로 전송
+        return chatMessage;
     }
 }
